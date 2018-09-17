1fc935ec6f71241e7c255acb98b69bb2b48f877b
Li Xi <lieinsteinxi@gmail.com>
2018-09-17 09:54:53 +0800

    added setup script to install to home directory

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='BPC203',
+      version='1.0',
+      description='BPC203 Piezoelectric actuator controller driver',
+      author='Li Xi',
+      author_email='lixi@lixiii.com',
+      url='https://github.com/lixiii/BPC203',
+      py_modules=['bpc203'],
+     )
